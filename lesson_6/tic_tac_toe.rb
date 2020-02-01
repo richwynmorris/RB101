1424783e34a249e831d99cc68f95b8c81dfb0c90
R Morris <you@example.com>
2020-02-01 15:48:09 +0000

    Updated tic-tac-toe after running through rubocop

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -11,6 +11,9 @@ require 'pry'
 # 9. If yes, go to #1
 # 10. Good bye!
 
+WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                     [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                     [[1, 5, 9], [3, 5, 7]] # diags
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -23,6 +26,7 @@ def intro
   prompt "Tic-Tac-Toe!"
 end
 
+# rubocop:disable Metics/AbcSize
 def display_board(brd)
   system 'clear'
   intro
@@ -40,28 +44,29 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metics/AbcSize
 
-def initialize_board 
+def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_spaces(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_pieces(brd)
   answer = ''
-  
-  loop do 
+
+  loop do
     prompt "Choose a square, (#{empty_spaces(brd).join(', ')})"
     answer = gets.chomp.to_i
     break if empty_spaces(brd).include?(answer)
     prompt "Sorry, that's not a valid choice."
   end
-    brd[answer] = PLAYER_MARKER
-end 
+  brd[answer] = PLAYER_MARKER
+end
 
 def computer_places_piece(brd)
   answer = empty_spaces(brd).sample
@@ -77,42 +82,39 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_conditions = [[1,2,3],[4,5,6],[7,8,9]] + #rows
-                       [[1,4,7],[2,5,8],[3,6,9]] + #cols
-                       [[1,5,9],[3,5,7]]  #diags
-                       
-  winning_conditions.each do |line|
+  WINNING_CONDITIONS.each do |line|
     if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER 
-       return 'Player'
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
     elsif brd[line[0]] == COMPUTER_MARKER &&
           brd[line[1]] == COMPUTER_MARKER &&
           brd[line[2]] == COMPUTER_MARKER
-          return 'Computer'
+      return 'Computer'
     end
   end
   nil
 end
 
 loop do
-board = initialize_board
-display_board(board)
-loop do
-  player_places_pieces(board)
-  computer_places_piece(board)
+  board = initialize_board
   display_board(board)
-  break if someone_won?(board) || board_full(board)
+  loop do
+    player_places_pieces(board)
+    computer_places_piece(board)
+    display_board(board)
+    break if someone_won?(board) || board_full(board)
+  end
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Want to play again? y for yes, n for no:"
+  answer = gets.chomp
+  break if answer.downcase != 'y'
 end
 
-if someone_won?(board)
-  prompt "#{detect_winner(board)} won!"
-else
-  prompt "It's a tie!"
-end
-
-prompt "Want to play again? y for yes, n for no:"
-answer = gets.chomp
-break if answer.downcase != 'y'
 prompt "Thanks for playing!"
-end
